59e178c70b2aa6cf1626973de0c588618cf85975
Jacek Hajto <72321150+20minadventure@users.noreply.github.com>
2023-02-22 05:25:48 +0100

    Add argument

--- analysis/helloworld.py
+++ analysis/helloworld.py
@@ -1,10 +1,16 @@
 # helloworld.py
+import sys
+
 import utils.init
 
 
 def hello():
+    if len(sys.argv) > 1):
+        n = int(sys.agrv[1])
+    else:
+        n = 0
     """Simple program that greets NAME for a total of COUNT times."""
-    for x in range(3):
+    for x in range(n):
         print('Hello dnax!')
 
 if __name__ == '__main__':
