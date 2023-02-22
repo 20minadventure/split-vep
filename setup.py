b11fdda6cb328076799fbd970e71878b737bf5b6
Jacek Hajto <72321150+20minadventure@users.noreply.github.com>
2023-02-22 19:15:49 +0100

    Remove some setup arguments

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import setup, find_packages
 setup(
     name="helloworld",
     version="2016.12.24",
-    description="",
-    long_description='README.md',
-    author="JH",
-    author_email="email@gmail.com",
-    url="",
+#    description="",
+#    long_description='README.md',
+#    author="JH",
+#    author_email="email@gmail.com",
+#    url="",
     packages=find_packages(),
-    license='LICENSE',
+#    license='LICENSE',
     entry_points={
         'console_scripts': [
             'aloha = analysis.helloworld:hello'
