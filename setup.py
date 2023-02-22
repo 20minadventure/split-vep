a663f67f367e83bff5a86ffbe29ac477f8deb57a
Jacek Hajto <72321150+20minadventure@users.noreply.github.com>
2023-02-22 21:37:54 +0100

    fix

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
 #    url="",
     packages=find_packages(),
 #    license='LICENSE',
-    data_files={'split-vep': ['utils/file']},
+    data_files={'split_vep': ['utils/file']},
     include_package_data=True,
     entry_points={
         'console_scripts': [
