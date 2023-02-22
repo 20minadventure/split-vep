c207dcf1eb4c7d90bd8ab427d2a9ecebdbb864ab
Jacek Hajto <72321150+20minadventure@users.noreply.github.com>
2023-02-22 21:57:03 +0100

    fix

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
 #    url="",
     packages=find_packages(),
 #    license='LICENSE',
-    package_data={'helloworld': ['utils/file']},
+#    package_data={'helloworld': ['utils/file']},
     include_package_data=True,
     entry_points={
         'console_scripts': [
