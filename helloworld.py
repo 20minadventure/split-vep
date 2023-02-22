8383bcfca76bad99b13bb7e3a9a02595e325b745
Jacek Hajto <72321150+20minadventure@users.noreply.github.com>
2023-02-22 04:42:17 +0100

    init

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+# helloworld.py
+def hello():
+    """Simple program that greets NAME for a total of COUNT times."""
+    for x in range(3):
+        print('Hello dnax!')
+
+if __name__ == '__main__':
+    hello()
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+
+setup(
+    name="helloworld",
+    version="2016.12.24",
+    description="",
+    long_description='README.md',
+    author="JH",
+    author_email="email@gmail.com",
+    url="",
+    py_modules=['helloworld'],
+    license='LICENSE',
+    entry_points={
+        'console_scripts': [
+            'aloha = helloworld:hello'
+        ]
+    },
+)
